a6b2b5a310f94ce3d043e1a5f1c12b2ed85bce54
asztrik <asztrikb@gmail.com>
2016-05-31 00:49:44 +0200

    analytics query part

---
+++ vfma/src/vfma/RawSensorData.java
@@ -0,0 +1,35 @@
+package vfma;
+
+public class RawSensorData {
+	
+		private String timestamp;
+		
+		private String sensorId;
+		
+		private String passing;
+		
+		public String getTimestamp() {
+			return timestamp;
+		}
+
+		public void setTimestamp(String timestamp) {
+			this.timestamp = timestamp;
+		}
+
+		public String getSensorId() {
+			return sensorId;
+		}
+
+		public void setSensorId(String sensorId) {
+			this.sensorId = sensorId;
+		}
+
+		public String getPassing() {
+			return passing;
+		}
+
+		public void setPassing(String passing) {
+			this.passing = passing;
+		}
+			
+}
